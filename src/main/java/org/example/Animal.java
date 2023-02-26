d4fb4e6dcc5721e68b3713625b01c5dc389cb314
Alona Mandziuk <alona.mandziuk@privatbank.ua>
2023-02-26 18:25:40 +0200

    commit

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,59 @@
+package org.example;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Animal {
+@Id
+private Integer id;
+private String type;
+private int age;
+private String name;
+private boolean tail;
+
+    public Animal() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isTail() {
+        return tail;
+    }
+
+    public void setTail(boolean tail) {
+        this.tail = tail;
+    }
+}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,51 @@
 package org.example;
 
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
 public class Main {
     public static void main(String[] args) {
+        Animal animal1 = new Animal();
+        animal1.setId(1);
+        animal1.setType("dog");
+        animal1.setName("Pepper");
+        animal1.setAge(7);
+        animal1.setTail(true);
+
+        Animal animal2 = new Animal();
+        animal2.setId(2);
+        animal2.setType("cat");
+        animal2.setName("Fluffy");
+        animal2.setAge(3);
+        animal2.setTail(true);
+
+        Animal animal3 = new Animal();
+        animal3.setId(3);
+        animal3.setType("hamster");
+        animal3.setName("Jerry");
+        animal3.setAge(1);
+        animal3.setTail(false);
+
+
+
+        SessionFactory factory = new Configuration()
+                .configure()
+                .buildSessionFactory();
+
+        Session session = factory.openSession();
+
+        session.beginTransaction();
+        session.save(animal1);
+        session.save(animal2);
+        session.save(animal3);
+        session.getTransaction().commit();
+        session.close();
+
 
 
     }
+
 }
